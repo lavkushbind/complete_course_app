7948aed83626ae1664d6a2deae941588376c1603
Lavkush <118399935+lavkushbind@users.noreply.github.com>
2023-09-09 08:33:09 +0530

    Update CheckoutActivity.java

--- CheckoutActivity.java
+++ CheckoutActivity.java
@@ -25,12 +25,9 @@ import org.json.JSONObject;
 
 import com.example.advncd.databinding.ActivityCheckoutBinding;
 
-/**
- * Checkout implementation for the app
- */
+
 public class CheckoutActivity extends AppCompatActivity {
 
-    // Arbitrarily-picked constant integer you define to track a request for payment data activity.
     private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 991;
 
     private CheckoutViewModel model;
@@ -38,11 +35,7 @@ public class CheckoutActivity extends AppCompatActivity {
     private ActivityCheckoutBinding layoutBinding;
     private View googlePayButton;
 
-    /**
-     * Initialize the Google Pay API on creation of the activity
-     *
-     * @see AppCompatActivity#onCreate(android.os.Bundle)
-     */
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,23 +47,14 @@ public class CheckoutActivity extends AppCompatActivity {
 
     private void initializeUi() {
 
-        // Use view binding to access the UI elements
         layoutBinding = ActivityCheckoutBinding.inflate(getLayoutInflater());
         setContentView(layoutBinding.getRoot());
 
-        // The Google Pay button is a layout file – take the root view
         googlePayButton = layoutBinding.googlePayButton.getRoot();
         googlePayButton.setOnClickListener(this::requestPayment);
     }
 
-    /**
-     * If isReadyToPay returned {@code true}, show the button and hide the "checking" text.
-     * Otherwise, notify the user that Google Pay is not available. Please adjust to fit in with
-     * your current user flow. You are not required to explicitly let the user know if isReadyToPay
-     * returns {@code false}.
-     *
-     * @param available isReadyToPay API response.
-     */
+   
     private void setGooglePayAvailable(boolean available) {
         if (available) {
             googlePayButton.setVisibility(View.VISIBLE);
@@ -81,36 +65,22 @@ public class CheckoutActivity extends AppCompatActivity {
 
     public void requestPayment(View view) {
 
-        // Disables the button to prevent multiple clicks.
         googlePayButton.setClickable(false);
 
-        // The price provided to the API should include taxes and shipping.
-        // This price is not displayed to the user.
         long dummyPriceCents = 100;
         long shippingCostCents = 900;
         long totalPriceCents = dummyPriceCents + shippingCostCents;
         final Task<PaymentData> task = model.getLoadPaymentDataTask(totalPriceCents);
 
-        // Shows the payment sheet and forwards the result to the onActivityResult method.
         AutoResolveHelper.resolveTask(task, this, LOAD_PAYMENT_DATA_REQUEST_CODE);
     }
 
-    /**
-     * Handle a resolved activity from the Google Pay payment sheet.
-     *
-     * @param requestCode Request code originally supplied to AutoResolveHelper in requestPayment().
-     * @param resultCode  Result code returned by the Google Pay API.
-     * @param data        Intent from the Google Pay API containing payment or error data.
-     * @see <a href="https://developer.android.com/training/basics/intents/result">Getting a result
-     * from an Activity</a>
-     */
+    
     @SuppressWarnings("deprecation")
-    // Suppressing deprecation until `registerForActivityResult` can be used with the Google Pay API.
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
-            // value passed in AutoResolveHelper
             case LOAD_PAYMENT_DATA_REQUEST_CODE:
                 switch (resultCode) {
 
@@ -120,7 +90,6 @@ public class CheckoutActivity extends AppCompatActivity {
                         break;
 
                     case AppCompatActivity.RESULT_CANCELED:
-                        // The user cancelled the payment attempt
                         break;
 
                     case AutoResolveHelper.RESULT_ERROR:
@@ -129,27 +98,17 @@ public class CheckoutActivity extends AppCompatActivity {
                         break;
                 }
 
-                // Re-enables the Google Pay payment button.
                 googlePayButton.setClickable(true);
         }
     }
 
-    /**
-     * PaymentData response object contains the payment information, as well as any additional
-     * requested information, such as billing and shipping address.
-     *
-     * @param paymentData A response object returned by Google after a payer approves payment.
-     * @see <a href="https://developers.google.com/pay/api/android/reference/
-     * object#PaymentData">PaymentData</a>
-     */
+    
     private void handlePaymentSuccess(@Nullable PaymentData paymentData) {
         final String paymentInfo = paymentData.toJson();
 
         try {
             JSONObject paymentMethodData = new JSONObject(paymentInfo).getJSONObject("paymentMethodData");
-            // If the gateway is set to "example", no payment information is returned - instead, the
-            // token will only consist of "examplePaymentMethodToken".
-
+          
             final JSONObject tokenizationData = paymentMethodData.getJSONObject("tokenizationData");
             final String token = tokenizationData.getString("token");
             final JSONObject info = paymentMethodData.getJSONObject("info");
@@ -158,22 +117,13 @@ public class CheckoutActivity extends AppCompatActivity {
                     this, getString(R.string.payments_show_name, billingName),
                     Toast.LENGTH_LONG).show();
 
-            // Logging token string.
             Log.d("Google Pay token: ", token);
 
         } catch (JSONException e) {
             throw new RuntimeException("The selected garment cannot be parsed from the list of elements");
         }
     }
-
-    /**
-     * At this stage, the user has already seen a popup informing them an error occurred. Normally,
-     * only logging is required.
-     *
-     * @param status will hold the value of any constant from CommonStatusCode or one of the
-     *               WalletConstants.ERROR_CODE_* constants.
-     * @see <a href="https://developers.google.com/android/reference/com/google/android/gms/wallet/
-     * WalletConstants#constant-summary">Wallet Constants Library</a>
+alletConstants#constant-summary">Wallet Constants Library</a>
      */
     private void handleError(@Nullable Status status) {
         String errorString = "Unknown error.";
